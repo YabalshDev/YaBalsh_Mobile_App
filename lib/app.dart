97aaa4160dc477fac35229026f4e2d56dc73e35d
devmoaid1 <moaidmohamed1312@gmail.com>
2022-11-30 15:12:03 +0200

    added routes

--- lib/app.dart
+++ lib/app.dart
@@ -1,25 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:yabalash_mobile_app/core/constants/app_strings.dart';
+import 'package:yabalash_mobile_app/routes/app_routes.dart';
 
 class YaBalashApp extends StatelessWidget {
   const YaBalashApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        title: AppStrings.appName,
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        home: const HomeView());
-  }
-}
-
-class HomeView extends StatelessWidget {
-  const HomeView({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const Scaffold();
+    return GetMaterialApp(
+      navigatorKey: Get.key,
+      getPages: RouteHelper.routes,
+      initialRoute: RouteHelper.getHomeRoute(),
+      title: AppStrings.appName,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+    );
   }
 }
