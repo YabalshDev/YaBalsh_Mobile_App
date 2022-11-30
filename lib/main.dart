f48c121b27a845e34d3e247242536d1ae4812048
devmoaid1 <moaidmohamed1312@gmail.com>
2022-11-30 15:21:43 +0200

    app works correctly

--- lib/main.dart
+++ lib/main.dart
@@ -3,5 +3,8 @@ import 'package:flutter/material.dart';
 import 'app.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  // intialize dependecies
+  // intialize hive boxes
   runApp(const YaBalashApp());
 }
