3e540e3f9a09d7a54f582c52e23ed6887cc73a1e
devmoaid1 <moaidmohamed1312@gmail.com>
2022-12-03 18:52:36 +0200

    added use cases

---
+++ lib/features/home/domain/usecases/get_sections_use_case.dart
@@ -0,0 +1,26 @@
+import 'package:dartz/dartz.dart';
+import 'package:equatable/equatable.dart';
+import 'package:yabalash_mobile_app/features/home/domain/entities/section.dart';
+
+import '../../../../core/errors/faliures.dart';
+import '../../../../core/usecases/use_cases.dart';
+import '../repositories/home_repository.dart';
+
+class GetSectiosParamsUseCase
+    implements UseCase<List<Section>, GetSectiosParams> {
+  final HomeRepository homeRepository;
+
+  GetSectiosParamsUseCase({required this.homeRepository});
+  @override
+  Future<Either<Failure, List<Section>>> call(GetSectiosParams params) =>
+      homeRepository.getHomeSections(keword: params.keword);
+}
+
+class GetSectiosParams extends Equatable {
+  final String keword;
+
+  const GetSectiosParams({required this.keword});
+  @override
+  // TODO: implement props
+  List<Object?> get props => [keword];
+}
