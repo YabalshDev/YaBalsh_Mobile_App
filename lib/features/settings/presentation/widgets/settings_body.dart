e856bc78ab9fa3a135b9a59601a0fe0aedbeb73b
devmoaid1 <moaidmohamed1312@gmail.com>
2022-12-15 16:32:24 +0200

    done settings screen

--- lib/features/settings/presentation/widgets/settings_body.dart
+++ lib/features/settings/presentation/widgets/settings_body.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:yabalash_mobile_app/core/constants/app_assets.dart';
+import 'package:yabalash_mobile_app/core/theme/light/app_colors_light.dart';
 import 'package:yabalash_mobile_app/features/settings/presentation/widgets/settings_element_card.dart';
 
 import '../../../../core/constants/app_layouts.dart';
@@ -13,59 +14,76 @@ class SettingsBody extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
       child: SingleChildScrollView(
-        child: Padding(
-          padding: kDefaultPadding,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const CustomHeader(isWithNotification: false, title: 'الاعدادات'),
-              largeVerticalSpace,
-              Text(
-                'حسابي',
-                style: Theme.of(context)
-                    .textTheme
-                    .bodyMedium
-                    ?.copyWith(fontSize: 14.sp, fontWeight: FontWeight.w700),
+        child: Column(
+          children: [
+            Padding(
+              padding: kDefaultPadding,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const CustomHeader(
+                      isWithNotification: false, title: 'الاعدادات'),
+                  largeVerticalSpace,
+                  Text(
+                    'حسابي',
+                    style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+                        fontSize: 14.sp, fontWeight: FontWeight.w700),
+                  ),
+                  largeVerticalSpace,
+                  SettingsElementCard(
+                      onTap: () {},
+                      iconPath: AppAssets.ordersIcon,
+                      title: 'طلباتي'),
+                  Divider(
+                    color: Colors.grey.shade300,
+                    thickness: 1,
+                  ),
+                  SettingsElementCard(
+                      onTap: () {},
+                      iconPath: AppAssets.addressesIcon,
+                      title: 'عناويني'),
+                  Divider(
+                    color: Colors.grey.shade300,
+                    thickness: 1,
+                  ),
+                  SettingsElementCard(
+                      onTap: () {},
+                      iconPath: AppAssets.headphoneIcon,
+                      title: 'مركز المساعدة'),
+                  Divider(
+                    color: Colors.grey.shade300,
+                    thickness: 1,
+                  ),
+                  SettingsElementCard(
+                      onTap: () {},
+                      iconPath: AppAssets.speakerphoneIcon,
+                      title: 'الشروط والاحكام'),
+                  Divider(
+                    color: Colors.grey.shade300,
+                    thickness: 1,
+                  ),
+                  SettingsElementCard(
+                      onTap: () {},
+                      iconPath: AppAssets.likeIcon,
+                      title: 'تابعنا علي صفحاتنا'),
+                ],
               ),
-              largeVerticalSpace,
-              SettingsElementCard(
+            ),
+            Container(
+              height: 10.h,
+              color: const Color(0xffF5F5F5),
+            ),
+            Padding(
+              padding: kDefaultPadding,
+              child: SettingsElementCard(
                   onTap: () {},
-                  iconPath: AppAssets.ordersIcon,
-                  title: 'طلباتي'),
-              Divider(
-                color: Colors.grey.shade300,
-                thickness: 1,
-              ),
-              SettingsElementCard(
-                  onTap: () {},
-                  iconPath: AppAssets.addressesIcon,
-                  title: 'عناويني'),
-              Divider(
-                color: Colors.grey.shade300,
-                thickness: 1,
-              ),
-              SettingsElementCard(
-                  onTap: () {},
-                  iconPath: AppAssets.ordersIcon,
-                  title: 'طلباتي'),
-              Divider(
-                color: Colors.grey.shade300,
-                thickness: 1,
-              ),
-              SettingsElementCard(
-                  onTap: () {},
-                  iconPath: AppAssets.ordersIcon,
-                  title: 'طلباتي'),
-              Divider(
-                color: Colors.grey.shade300,
-                thickness: 1,
-              ),
-              SettingsElementCard(
-                  onTap: () {},
-                  iconPath: AppAssets.ordersIcon,
-                  title: 'طلباتي'),
-            ],
-          ),
+                  isWithTrailing: false,
+                  titleColor: AppColorsLight.kAppPrimaryColorLight,
+                  iconColor: AppColorsLight.kAppPrimaryColorLight,
+                  iconPath: AppAssets.logoutIcon,
+                  title: 'تسجيل الخروج'),
+            )
+          ],
         ),
       ),
     );
--- lib/features/settings/presentation/widgets/settings_element_card.dart
+++ lib/features/settings/presentation/widgets/settings_element_card.dart
@@ -9,10 +9,15 @@ class SettingsElementCard extends StatelessWidget {
   final String iconPath;
   final String title;
   final bool? isPhoto;
-
+  final Color? titleColor;
+  final bool? isWithTrailing;
+  final Color? iconColor;
   const SettingsElementCard(
       {super.key,
       required this.onTap,
+      this.titleColor = Colors.black,
+      this.iconColor = Colors.black,
+      this.isWithTrailing = true,
       this.isPhoto = false,
       required this.iconPath,
       required this.title});
@@ -23,11 +28,13 @@ class SettingsElementCard extends StatelessWidget {
       height: 56.h,
       child: ListTile(
           onTap: onTap,
-          trailing: Icon(
-            Icons.arrow_forward_ios,
-            color: Colors.grey,
-            size: 15.h,
-          ),
+          trailing: isWithTrailing!
+              ? Icon(
+                  Icons.arrow_forward_ios,
+                  color: Colors.grey,
+                  size: 15.h,
+                )
+              : null,
           title: Row(
             children: [
               isPhoto!
@@ -41,13 +48,14 @@ class SettingsElementCard extends StatelessWidget {
                       iconPath: iconPath,
                       width: 15.w,
                       height: 16.h,
+                      color: iconColor,
                     ),
               mediumHorizontalSpace,
-              Text('طلباتي',
-                  style: Theme.of(context)
-                      .textTheme
-                      .bodyMedium
-                      ?.copyWith(fontSize: 14.sp, fontWeight: FontWeight.w600)),
+              Text(title,
+                  style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+                      fontSize: 14.sp,
+                      fontWeight: FontWeight.w600,
+                      color: titleColor)),
             ],
           )),
     );
