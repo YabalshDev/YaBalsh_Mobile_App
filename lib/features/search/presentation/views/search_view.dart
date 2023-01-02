d3ad6c8588ba14d900e68ce5063cb356cc42f9e0
devmoaid1 <moaidmohamed1312@gmail.com>
2023-01-02 23:04:24 +0200

    dispose controllers

--- lib/features/search/presentation/views/search_view.dart
+++ lib/features/search/presentation/views/search_view.dart
@@ -2,20 +2,37 @@ import 'package:flutter/material.dart';
 
 import '../widgets/search_body.dart';
 
-final PageController _pageController = PageController();
-
-class SearchView extends StatelessWidget {
+class SearchView extends StatefulWidget {
   final String? intialValue;
   final bool fromCategory;
   const SearchView({super.key, this.intialValue, required this.fromCategory});
 
   @override
+  State<SearchView> createState() => _SearchViewState();
+}
+
+class _SearchViewState extends State<SearchView> {
+  late PageController pageController;
+  @override
+  void initState() {
+    pageController = PageController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    pageController.dispose();
+
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SearchBody(
-          pageController: _pageController,
-          fromCategory: fromCategory,
-          intialValue: intialValue),
+          pageController: pageController,
+          fromCategory: widget.fromCategory,
+          intialValue: widget.intialValue),
     );
   }
 }
